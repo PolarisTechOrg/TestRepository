cd4c457a61959c8f2a323e4ac866510babe2455b
admin <admin@admins-Mac-2.local>
2014-09-17 10:43:55 +0800

    jjj

---
+++ FcpAssistant/FcpAssistant/Strategy/FAStrategyViewCell.m
@@ -0,0 +1,23 @@
+//
+//  FAStrategyViewCell.m
+//  FcpAssistant
+//
+//  Created by admin on 9/17/14.
+//  Copyright (c) 2014 polaris. All rights reserved.
+//
+
+#import "FAStrategyViewCell.h"
+
+@implementation FAStrategyViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
