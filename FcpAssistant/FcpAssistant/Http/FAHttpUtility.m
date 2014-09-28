d7c73f3ea98a3131909355a2574e6eb20237d709
ShenHua <admin@admins-Mac-2.local>
2014-09-28 17:13:31 +0800

    update

--- FcpAssistant/FcpAssistant/Http/FAHttpUtility.m
+++ FcpAssistant/FcpAssistant/Http/FAHttpUtility.m
@@ -32,18 +32,30 @@
     [urlRequest setHTTPMethod:head.Method];
     
     NSDictionary *bodyDict = [FAJSONSerialization toDictionary:body];
+    NSMutableString *bodyString = [[NSMutableString alloc] init];
     
-    if(bodyDict != nil && [NSJSONSerialization isValidJSONObject:bodyDict])
+    for(NSString* key in bodyDict.allKeys)
     {
-        NSData *postData = [NSJSONSerialization dataWithJSONObject:bodyDict options:NSJSONWritingPrettyPrinted error:&error];
-        
-        NSNumber *length = [NSNumber numberWithLong:[postData length]];
-        NSString *postLength = [NSString stringWithFormat:@"%d", [length intValue]];
-        
-        [urlRequest setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Tppe"];
-        [urlRequest setValue:postLength forHTTPHeaderField:@"Content-Length"];
-        [urlRequest setHTTPBody:postData];
+        [bodyString appendString:key];
+        [bodyString appendString:@"="];
+        [bodyString appendFormat:@"%@",[bodyDict valueForKey:key]];
+        [bodyString appendString:@"&"];
     }
+    NSUInteger length = [bodyString length];
+    [bodyString deleteCharactersInRange:NSMakeRange(length-1, 1)];
+    [urlRequest setHTTPBody:[bodyString dataUsingEncoding:NSUTF8StringEncoding]];
+    
+//    if(bodyDict != nil && [NSJSONSerialization isValidJSONObject:bodyDict])
+//    {
+//        NSData *postData = [NSJSONSerialization dataWithJSONObject:bodyDict options:NSJSONWritingPrettyPrinted error:&error];
+    
+//        NSNumber *length = [NSNumber numberWithLong:[postData length]];
+//        NSString *postLength = [NSString stringWithFormat:@"%d", [length intValue]];
+//        
+//        [urlRequest setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Tppe"];
+//        [urlRequest setValue:postLength forHTTPHeaderField:@"Content-Length"];
+//        [urlRequest setHTTPBody:postData];
+//    }
     
     NSData *retData;
     NSHTTPURLResponse *response;
