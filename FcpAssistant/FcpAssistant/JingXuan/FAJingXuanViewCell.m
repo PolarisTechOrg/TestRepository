67d7b3f164f00fef1944c7489788f698cdb301b3
ShenHua <admin@admins-Mac-2.local>
2014-10-11 10:57:59 +0800

    update jingxuan event

--- FcpAssistant/FcpAssistant/JingXuan/FAJingXuanViewCell.m
+++ FcpAssistant/FcpAssistant/JingXuan/FAJingXuanViewCell.m
@@ -12,6 +12,14 @@
 
 @implementation FAJingXuanViewCell
 
+@synthesize nameIdDict;
+@synthesize navigationController;
+
+@synthesize strategyId1;
+@synthesize strategyId2;
+@synthesize strategyId3;
+
+
 - (void)awakeFromNib {
     // Initialization code
 }
@@ -22,17 +30,21 @@
     // Configure the view for the selected state
 }
 
+// after cell initialization
+- (void)initialNameIdDictionary
+{
+    nameIdDict = [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithInt:strategyId1], _btnStrategyName1.titleLabel.text, [NSNumber numberWithInt:strategyId2], _btnStrategyName2.titleLabel.text, [NSNumber numberWithInt:strategyId3], _btnStrategyName3.titleLabel.text, nil];
+}
+
 - (IBAction)strategyNamePressed:(id)sender
 {
     UIButton *btn = (UIButton *)sender;
     NSString *txt = btn.titleLabel.text;
     
-    UITableView *superView = (UITableView *)self.superview;
-    FAJingXuanController *superController = (FAJingXuanController *)superView.dataSource;
     
     FAStrategyDetailController *controller = [[FAStrategyDetailController alloc] init];
-    controller.strategyId = 377;
+    controller.strategyId = [[self.nameIdDict objectForKey:txt] intValue];
     controller.hidesBottomBarWhenPushed = YES;
-    [superController.navigationController pushViewController:controller animated:YES];
+    [self.navigationController pushViewController:controller animated:YES];
 }
 @end
