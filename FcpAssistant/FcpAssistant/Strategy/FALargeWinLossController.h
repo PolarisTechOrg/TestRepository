20ea3a0870a463b946aaaf43f4058a1c65e59375
ShenHua <admin@admins-Mac-2.local>
2014-10-16 17:35:32 +0800

    add large map controller

---
+++ FcpAssistant/FcpAssistant/Strategy/FALargeWinLossController.h
@@ -0,0 +1,13 @@
+//
+//  FALargeWinLossController.h
+//  FcpAssistant
+//
+//  Created by admin on 10/16/14.
+//  Copyright (c) 2014 polaris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FALargeWinLossController : UIViewController
+
+@end
