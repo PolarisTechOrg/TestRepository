52d44adf56607cc4e003fcd79eaa215bb845fa9c
ShenHua <admin@admins-Mac-2.local>
2014-11-18 14:55:18 +0800

    update message edit

--- FcpAssistant/FcpAssistant/Message/DetailView/FAMessageDetailViewCell2.m
+++ FcpAssistant/FcpAssistant/Message/DetailView/FAMessageDetailViewCell2.m
@@ -24,10 +24,34 @@
         if (selected)
         {
             NSLog(@"cell selected tag:%ld", self.tag);
+            
+            if ([self viewWithTag:self.tag])
+            {
+                UIImage *image = [UIImage imageNamed:@"message_radio_2"];
+                UIImageView *editView = [[UIImageView alloc] initWithImage:image];
+                editView.tag = self.tag;
+                editView.frame = CGRectMake(12, 29, 22, 22);
+                [self addSubview:editView];
+            }
         }
         else
         {
             NSLog(@"cell deSelected tag:%ld", self.tag);
+            
+            if (!self.subviews[0])
+            {
+                return;
+            }
+            UIView *editView = (UIView *)self.subviews[0];
+            
+            for (UIView *subView in editView.subviews)
+            {
+                if(subView.tag == self.tag)
+                {
+                    [subView removeFromSuperview];
+                }
+            }
+            
         }
     }
 }
@@ -36,30 +60,30 @@
 {
     [super setEditing:editing animated:animated];
     
-    if (editing)
-    {
-        if(self.editingStyle == (UITableViewCellEditingStyleInsert | UITableViewCellEditingStyleDelete))
-        {
-            if (![self viewWithTag:self.tag])
-            {
-                UIImage *image = [UIImage imageNamed:@""];
-                UIImageView *editView = [[UIImageView alloc] initWithImage:image];
-                editView.tag = self.tag;
-                editView.frame = CGRectMake(10, 15, 20, 20);
-                [self addSubview:editView];
-            }
-        }
-        NSLog(@"cell editing tag:%ld", self.tag);
-    }
-    else
-    {
+//    if (editing)
+//    {
+//        if(self.editingStyle == (UITableViewCellEditingStyleInsert | UITableViewCellEditingStyleDelete))
+//        {
+//            if (![self viewWithTag:self.tag])
+//            {
+//                UIImage *image = [UIImage imageNamed:@""];
+//                UIImageView *editView = [[UIImageView alloc] initWithImage:image];
+//                editView.tag = self.tag;
+//                editView.frame = CGRectMake(10, 15, 20, 20);
+//                [self addSubview:editView];
+//            }
+//        }
+//        NSLog(@"cell editing tag:%ld", self.tag);
+//    }
+//    else
+//    {
 //        UIView *editView = [self viewWithTag:self.tag];
 //        if(editView)
 //        {
 //            [editView removeFromSuperview];
 //        }
-        NSLog(@"cell noEditing tag:%ld", self.tag);
-    }
+//        NSLog(@"cell noEditing tag:%ld", self.tag);
+//    }
 }
 
 @end
