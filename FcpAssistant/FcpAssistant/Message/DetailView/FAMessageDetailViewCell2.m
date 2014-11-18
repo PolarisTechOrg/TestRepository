d9012a3846ac43c5680ed6898af7ef5c50c412f3
ShenHua <admin@admins-Mac-2.local>
2014-11-18 15:07:31 +0800

    update message edit

--- FcpAssistant/FcpAssistant/Message/DetailView/FAMessageDetailViewCell2.m
+++ FcpAssistant/FcpAssistant/Message/DetailView/FAMessageDetailViewCell2.m
@@ -31,6 +31,7 @@
                 UIImageView *editView = [[UIImageView alloc] initWithImage:image];
                 editView.tag = self.tag;
                 editView.frame = CGRectMake(12, 29, 22, 22);
+                
                 [self addSubview:editView];
             }
         }
@@ -46,7 +47,7 @@
             
             for (UIView *subView in editView.subviews)
             {
-                if(subView.tag == self.tag)
+                if(subView.tag == self.tag && [subView isKindOfClass:[UIImageView class]])
                 {
                     [subView removeFromSuperview];
                 }
@@ -56,34 +57,9 @@
     }
 }
 
-- (void)setEditing:(BOOL)editing animated:(BOOL)animated
-{
-    [super setEditing:editing animated:animated];
-    
-//    if (editing)
-//    {
-//        if(self.editingStyle == (UITableViewCellEditingStyleInsert | UITableViewCellEditingStyleDelete))
-//        {
-//            if (![self viewWithTag:self.tag])
-//            {
-//                UIImage *image = [UIImage imageNamed:@""];
-//                UIImageView *editView = [[UIImageView alloc] initWithImage:image];
-//                editView.tag = self.tag;
-//                editView.frame = CGRectMake(10, 15, 20, 20);
-//                [self addSubview:editView];
-//            }
-//        }
-//        NSLog(@"cell editing tag:%ld", self.tag);
-//    }
-//    else
-//    {
-//        UIView *editView = [self viewWithTag:self.tag];
-//        if(editView)
-//        {
-//            [editView removeFromSuperview];
-//        }
-//        NSLog(@"cell noEditing tag:%ld", self.tag);
-//    }
-}
+//- (void)setEditing:(BOOL)editing animated:(BOOL)animated
+//{
+//    [super setEditing:editing animated:animated];
+//}
 
 @end
