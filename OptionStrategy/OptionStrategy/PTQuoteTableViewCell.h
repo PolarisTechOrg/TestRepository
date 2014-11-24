f172bd6ea85f02968fe863ff749c489ea23eda32
YuXiaoLiang <user01@admins-Mac.local>
2014-11-24 11:12:45 +0800

    Create Quote.storyboard

---
+++ OptionStrategy/OptionStrategy/PTQuoteTableViewCell.h
@@ -0,0 +1,25 @@
+//
+//  PTQuoteTableViewCell.h
+//  OptionStrategy
+//
+//  Created by user01 on 11/24/14.
+//  Copyright (c) 2014年 Polaris Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTQuoteTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIButton *btCallBackGroud;
+@property (weak, nonatomic) IBOutlet UIButton *btCallRadio;
+@property (weak, nonatomic) IBOutlet UILabel *lblCallBuyOne;
+@property (weak, nonatomic) IBOutlet UILabel *lblCallSaleOne;
+@property (weak, nonatomic) IBOutlet UILabel *lblCallVolatility;
+@property (weak, nonatomic) IBOutlet UIButton *btPutBackGroud;
+@property (weak, nonatomic) IBOutlet UIButton *btPutRadio;
+@property (weak, nonatomic) IBOutlet UILabel *lblStrikePrice;
+@property (weak, nonatomic) IBOutlet UILabel *lblPutBuyOne;
+@property (weak, nonatomic) IBOutlet UILabel *lblPutSaleOne;
+@property (weak, nonatomic) IBOutlet UILabel *lblPutVolatility;
+
+@end
---
+++ OptionStrategy/OptionStrategy/PTQuoteTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PTQuoteTableViewController.h
+//  OptionStrategy
+//
+//  Created by user01 on 11/24/14.
+//  Copyright (c) 2014年 Polaris Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTQuoteTableViewController : UITableViewController
+
+@end
