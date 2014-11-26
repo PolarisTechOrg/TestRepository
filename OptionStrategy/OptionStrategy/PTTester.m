e3ebaba81ba3e4ee5d200a3ff93fcd5f4dafda29
jacksontjl <jackytong from polaristech>
2014-11-26 13:30:29 +0800

    add tester file.s

---
+++ OptionStrategy/OptionStrategy/PTTester.m
@@ -0,0 +1,13 @@
+//
+//  PTTester.m
+//  OptionStrategy
+//
+//  Created by admin on 11/26/14.
+//  Copyright (c) 2014 Polaris Technology. All rights reserved.
+//
+
+#import "PTTester.h"
+
+@implementation PTTester
+
+@end
